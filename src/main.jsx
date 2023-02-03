d88ad34d5fc5825e3e3dc2dfc53012e9cf1f8e65
S2ATech <98845575+S2ATech@users.noreply.github.com>
2023-02-03 08:33:08 +0100

    5 files modified

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import '@fontsource/roboto/700.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+//import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
